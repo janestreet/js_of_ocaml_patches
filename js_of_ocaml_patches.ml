247a7dd45b610907d2dc00eb0db03f0f60920efa
public-release <as-public-release@janestreet.com>
2023-03-23 18:15:22 +0000

    v0.16~preview.128.12+239

--- js_of_ocaml_patches.ml
+++ js_of_ocaml_patches.ml
@@ -3,12 +3,11 @@ module Dom_html = struct
   open Js_of_ocaml.Js
 
   (* No js_of_ocaml PR yet. *)
-  class type wheelEvent =
-    object
-      inherit mouseEvent
-      method deltaX : float readonly_prop
-      method deltaY : float readonly_prop
-      method deltaZ : float readonly_prop
-      method deltaMode : int readonly_prop
-    end
+  class type wheelEvent = object
+    inherit mouseEvent
+    method deltaX : float readonly_prop
+    method deltaY : float readonly_prop
+    method deltaZ : float readonly_prop
+    method deltaMode : int readonly_prop
+  end
 end
--- js_of_ocaml_patches.mli
+++ js_of_ocaml_patches.mli
@@ -2,12 +2,11 @@ module Dom_html : sig
   open Js_of_ocaml.Dom_html
   open Js_of_ocaml.Js
 
-  class type wheelEvent =
-    object
-      inherit mouseEvent
-      method deltaX : float readonly_prop
-      method deltaY : float readonly_prop
-      method deltaZ : float readonly_prop
-      method deltaMode : int readonly_prop
-    end
+  class type wheelEvent = object
+    inherit mouseEvent
+    method deltaX : float readonly_prop
+    method deltaY : float readonly_prop
+    method deltaZ : float readonly_prop
+    method deltaMode : int readonly_prop
+  end
 end
