3b42d952dfba79b987d7cc2087b9980fe2859242
public-release <as-public-release@janestreet.com>
2025-04-28 19:35:05 +0100

    v0.18~preview.130.26+1192

--- js_of_ocaml_patches.mli
+++ js_of_ocaml_patches.mli
@@ -2,46 +2,6 @@ module Dom_html : sig
   open Js_of_ocaml.Dom_html
   open Js_of_ocaml.Js
 
-  class type mouseEvent = object
-    inherit event
-    method relatedTarget : element t opt optdef readonly_prop
-    method clientX : number_t readonly_prop
-    method clientY : number_t readonly_prop
-    method screenX : number_t readonly_prop
-    method screenY : number_t readonly_prop
-    method offsetX : number_t readonly_prop
-    method offsetY : number_t readonly_prop
-    method ctrlKey : bool t readonly_prop
-    method shiftKey : bool t readonly_prop
-    method altKey : bool t readonly_prop
-    method metaKey : bool t readonly_prop
-    method button : int readonly_prop
-    method which : mouse_button optdef readonly_prop
-    method fromElement : element t opt optdef readonly_prop
-    method toElement : element t opt optdef readonly_prop
-    method pageX : number_t optdef readonly_prop
-    method pageY : number_t optdef readonly_prop
-  end
-
-  class type pointerEvent = object
-    inherit mouseEvent
-    method pointerId : int readonly_prop
-    method width : number_t readonly_prop
-    method height : number_t readonly_prop
-    method pressure : number_t readonly_prop
-    method tangentialPressure : number_t readonly_prop
-    method tiltX : int readonly_prop
-    method tiltY : int readonly_prop
-    method twist : int readonly_prop
-    method pointerType : js_string t readonly_prop
-    method isPrimary : bool t readonly_prop
-  end
-
-  class type wheelEvent = object
-    inherit mouseEvent
-    method deltaX : number t readonly_prop
-    method deltaY : number t readonly_prop
-    method deltaZ : number t readonly_prop
-    method deltaMode : int readonly_prop
-  end
+  (* https://developer.mozilla.org/en-US/docs/Web/API/Event/stopImmediatePropagation *)
+  val stopImmediatePropagation : ?here:Stdlib.Lexing.position -> #event t -> unit
 end
